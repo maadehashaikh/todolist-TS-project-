2f0ac4bc9af45e11f4cd4a900b2f178217633c61
Maadeha shaikh <maadeham@gmail.com>
2024-04-09 01:23:32 +0500

    changes whole code

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
-var a = 3;
-var b = 4;
-console.log(a * b);
+import inquirer from "inquirer";
+const todos = [];
+let loop = true;
+while (loop) {
+    const answer = await inquirer.prompt([
+        {
+            type: "input",
+            name: "TODO",
+            message: "What Do You Wanna Add In Your ToDo ?"
+        },
+        {
+            type: "confirm",
+            name: "addMore",
+            message: "Do You Wanna Add More Todo",
+            default: false
+        }
+    ]);
+    const { TODO, addMore } = answer;
+    loop = addMore;
+    if (TODO) {
+        todos.push(TODO);
+    }
+    else {
+        console.log("kindly add valid input");
+    }
+}
+if (todos.length > 0) {
+    console.log("Your To do list: \n ");
+    todos.forEach(todo => {
+        console.log(todo);
+    });
+}
+else {
+    console.log("No Todos Found !");
+}
--- main.ts
+++ main.ts
@@ -1,3 +1,38 @@
-let a=3;
-let b=4;
-console.log(a*b);
+import inquirer from "inquirer";
+const todos :string[] = [];
+let loop = true;
+while(loop){
+  const answer : {
+    TODO:string,
+    addMore:boolean
+  } = await inquirer.prompt([
+    {
+      type:"input",
+      name:"TODO",
+      message:"What Do You Wanna Add In Your ToDo ?"
+    },
+    {
+      type:"confirm", // it will take answer as y or n
+      name:"addMore",
+      message:"Do You Wanna Add More Todo",
+      default:false
+    }
+  ])
+  const {TODO ,addMore} =answer;
+  loop =  addMore;
+  if(TODO){
+     todos.push(TODO)
+  }
+  else{
+    console.log("kindly add valid input");
+  }
+}
+if(todos.length >0){
+  console.log("Your To do list: \n ")
+   todos.forEach(todo =>{
+    console.log(todo);
+   })
+}
+else{
+  console.log("No Todos Found !");
+}
